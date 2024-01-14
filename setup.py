9cb9591194cade08bd9b52abe045214a0dfb3fab
quojus <60073916+quojus@users.noreply.github.com>
2024-01-14 20:21:33 +0100

    brum

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='histoColorbar',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     description='Histogramm mit Farbskala in Matplotlib',
     long_description=open('README.md').read(),
